86bfecdb4052712de49146e7ed1a34aaaa899d37
andresurbano <andresx1000@hotmail.com  git config --global user.name andresurbano git config --global user.email andresx1000@hotmail.com>
2023-10-11 01:38:32 -0300

    Entrega Poli

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var swiper1 = new Swiper('.mySwiper-1', {
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        pagination: {
+            el: '.swiper-pagination',
+        },
+    });
 
+    var swiper2 = new Swiper('.mySwiper-2', {
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        pagination: {
+            el: '.swiper-pagination',
+        },
+    });
+});
